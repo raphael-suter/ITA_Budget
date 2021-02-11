af6da1f4a819caa944eba407ea503daa7d630ef6
Raphael Suter <raphael.werner.suter@gmail.com>
2021-02-11 17:01:41 +0100

    ...

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -3,10 +3,11 @@ import { Text } from "react-native";
 import { DataContext } from "../Data/Data";
 import { List, ListItem } from "native-base";
 import MonthPicker from "../Components/MonthPicker";
+import moment from "moment";
 
 const HomeScreen = () => {
   const { budget, setBudget } = useContext(DataContext);
-  const [month, setMonth] = useState(0);
+  const [month, setMonth] = useState(moment().format("M") - 1);
 
   return (
     <>
